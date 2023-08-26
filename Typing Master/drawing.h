7e81c940f7e97125dbdeed6e936ac05a92c64ea6
CodeWithIsmail <hmismail360@gmail.com>
2023-08-26 08:36:56 +0600

    drawing in console window

---
+++ Typing Master/drawing.h
@@ -0,0 +1,11 @@
+#ifndef DRAWING_H_INCLUDED
+#define DRAWING_H_INCLUDED
+#include<bits/stdc++.h>
+using namespace std;
+
+void drawKeyboard();
+void drawHistogram(char dataset[],int size,int frequency[]);
+void drawBorder(int x1,int x2,int y1,int y2,string ch1,string ch2);
+
+
+#endif // DRAWING_H_INCLUDED
