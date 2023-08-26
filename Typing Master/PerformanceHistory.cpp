3e2e7d182bba42d63ef67021f866a26805d4894f
CodeWithIsmail <hmismail360@gmail.com>
2023-08-26 08:43:36 +0600

    performance history stored in file

--- Typing Master/PerformanceHistory.cpp
+++ Typing Master/PerformanceHistory.cpp
@@ -1,8 +1,32 @@
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 #include<windows.h>
 #include<conio.h>
 #include<fstream>
+#include "set_cursor.h"
+#include "PerformanceHistory.h"*/
+#include "AllHeaderFile.h"
+
+using namespace std;
 void history()
 {
+    system("cls");
+    gotoxy(5,5);
+    ifstream username("UserList.txt");
+    string name;
+    getline(username,name);
+    username.close();
 
+    cout<<"\t User: "<<name<<endl;
+    cout<<"\t______________________________________________________________________________________________________________________\n\n";
+
+    cout<<"\t Tutorial Number \t\t Date \t\t Total Key Press \t Wrong Key Press \t Accuracy \t Score\n\n";
+    cout<<"\t_______________________________________________________________________________________________________________________\n\n";
+
+    ifstream performance("PerformanceHistory.txt");
+    string line;
+    while(getline(performance,line))
+    {
+       cout<<"\t "<<line<<endl;
+    }
+    performance.close();
 }
