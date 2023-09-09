9675576c2265b9564bdd68ba6b0eec2316b8b49b
CodeWithIsmail <hmismail360@gmail.com>
2023-09-09 19:38:10 +0600

    user manual created

---
+++ Typing Master/manual.cpp
@@ -0,0 +1,23 @@
+#include "AllHeaderFile.h"
+
+void UserManual()
+{
+    system("cls");
+    cout<<"\n\n\n-----------------------------------------------------------------------------------------------------------------------------------------------------------";
+    cout<<"\n\n\t\t\t\t\t\t\tWelcome to Typing Master";
+    cout<<"\n\n\t\t\t\t\t\tAn Interactive Typing Tutor and Typing Game";
+    cout<<"\n\n------------------------------------------------------------------------------------------------------------------------------------------------------------";
+
+    cout<<"\n\n";
+    cout<<"\n\t\t\t\t------------------------         \t\t------------------------         ---------------------------";
+    cout<<"\n\t\t\t\t|   Typing Tutorial    |         \t\t|   Typing Game        |         |   Typing Statistics     |";
+    cout<<"\n\t\t\t\t------------------------         \t\t------------------------         ---------------------------";
+    cout<<"\n\t\t\t\t\t|--> Lesson 1: Home Row Keys      \t\t|--> Fast Typer                  |--> Performance History";
+    cout<<"\n\t\t\t\t\t|--> Lesson 2: Index Finger Keys   \t\t|--> Word Typer                  |--> Performance Analysis";
+    cout<<"\n\t\t\t\t\t|--> Lesson 3: Middle Finger Keys";
+    cout<<"\n\t\t\t\t\t|--> Lesson 4: Ring Finger Keys";
+    cout<<"\n\t\t\t\t\t|--> Lesson 5: Little Finger Keys";
+    cout<<"\n\t\t\t\t\t|--> Lesson 6: Common Words";
+
+    cout<<"\n\n";
+}
