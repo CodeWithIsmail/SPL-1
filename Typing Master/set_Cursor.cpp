a7486c40df596da01b5afb0b8e73fce1a9520064
CodeWithIsmail <hmismail360@gmail.com>
2023-09-09 19:39:27 +0600

    final update on cursor move

--- Typing Master/set_Cursor.cpp
+++ Typing Master/set_Cursor.cpp
@@ -1,39 +1,14 @@
-/*#include "set_cursor.h"
-#include<windows.h>
-#include<bits/stdc++.h>
-*/
 #include "AllHeaderFile.h"
-using namespace std;
 
 HANDLE console=GetStdHandle(STD_OUTPUT_HANDLE);
 COORD cursorPoint;
-
-void gotoxy(int x,int y)
+void moveCursor(int x,int y)
 {
     cursorPoint.X=x;
     cursorPoint.Y=y;
     SetConsoleCursorPosition(console,cursorPoint);
 }
 
-void setColor(int colorNum)
-{
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),colorNum);
-}
-
-void setConsoleBackgroundColor(WORD color)
-{
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    CONSOLE_SCREEN_BUFFER_INFOEX info;
-    info.cbSize = sizeof(CONSOLE_SCREEN_BUFFER_INFOEX);
-    GetConsoleScreenBufferInfoEx(hConsole, &info);
-
-    // Set the background color for the console
-    info.ColorTable[0] = color; // Change the background color to the provided color value
-    info.wAttributes = (info.wAttributes & 0xFFF0) | (color & 0x000F);
-
-    SetConsoleScreenBufferInfoEx(hConsole, &info);
-}
 
 
 
