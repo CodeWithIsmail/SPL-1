7ae03457eaac3811381a3836d38658902cfa92a7
CodeWithIsmail <hmismail360@gmail.com>
2023-09-09 11:40:49 +0600

    final update on drawing

--- Typing Master/drawing.cpp
+++ Typing Master/drawing.cpp
@@ -1,16 +1,9 @@
-/*
-#include<bits/stdc++.h>
-#include<conio.h>
-#include "set_cursor.h"
-#include"drawing.h"
-*/
 #include "AllHeaderFile.h"
-using namespace std;
 
 void drawKeyboard()
 {
-    gotoxy(0,20);
-    cout << " Keyboard :\n";
+    gotoxy(10,20);
+    cout <<"Keyboard :\n";
     cout <<
          "\t\t     ,---,---,---,---,---,---,---,---,---,---,---,---,---,-------'\n" <<
          "\t\t     | ~ | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | + |   <-  |\n" <<
@@ -27,26 +20,20 @@ void drawKeyboard()
 
 void drawHistogram(char dataset[],int size,int frequency[])
 {
-    cout<<endl;
-    cout<<"\t  Your difficult keys in this lesson:"<<endl;
-    cout<<endl;
+    cout<<"\n\t\tYour difficult keys in this lesson:\n\n";
+
     int maxFreq=INT_MIN;
     for(int i=0; i<size; i++)
     {
         if(frequency[i]>maxFreq)
-        {
             maxFreq=frequency[i];
-        }
     }
 
     for(int row=maxFreq; row>0; row--)
     {
         for(int element=0; element<size; element++)
         {
-            if(frequency[element]<row)
-                cout<<"           ";
-            else
-                cout<<" ********* ";
+            frequency[element]<row? cout<<"           " : cout<<" ********* ";
         }
         cout<<endl;
     }
@@ -67,7 +54,6 @@ void drawBorder(int x1,int x2,int y1,int y2,string ch1,string ch2)
         gotoxy(x1+i,y2);
         cout<<ch1;
     }
-
     for(int i=1; i<y2-y1; i++)
     {
         gotoxy(x1,y1+i);
@@ -75,6 +61,5 @@ void drawBorder(int x1,int x2,int y1,int y2,string ch1,string ch2)
         gotoxy(x2,y1+i);
         cout<<ch2;
     }
-
 }
 
