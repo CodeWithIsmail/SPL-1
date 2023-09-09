adda6443295613dd8d2582c5e7242118534011a1
CodeWithIsmail <hmismail360@gmail.com>
2023-09-09 11:27:59 +0600

    final update on Common word Practice

--- Typing Master/commonWords.cpp
+++ Typing Master/commonWords.cpp
@@ -1,42 +1,29 @@
-/*
-#include<bits/stdc++.h>
-#include<windows.h>
-#include<conio.h>
-#include<fstream>
-#include"set_cursor.h"
-#include"StartMenu.h"
-#include"commonWords.h"
-*/
-
 #include "AllHeaderFile.h"
-
 using namespace std;
 
 void mostCommonWord()
 {
-    int gross=0,wrong=0;
-    gotoxy(20,5);
-    cout<<"Accuracy goal 95%";
-    gotoxy(20,7);
-    cout<<"Objective: Learn to fluently type some of the most common English words";
-    gotoxy(20,10);
-    cout<<"Press any key to start.";
+    cout<<"\n\n\t\t\t Accuracy goal 95%";
+    cout<<"\n\n\t\t\t Objective: Learn to fluently type some of the most common English words\n\n";
+    cout<<"\n\n\t\t\t Press any key to start.\n\n";
     char ch;
     ch=getch();
-    system("cls");
 
-    int total=50,score=0;
+    system("cls");
+    drawBorder(40,65,3,7,"-","|");
+    drawKeyboard();
+
+    int gross=0,wrong=0,total=50,score=0;
 
     ifstream inputFile("MostCommonEnglishWord.txt");
-
     string out;
+
     while (getline(inputFile, out))
     {
-
-        system("cls");
-        drawBorder(40,65,3,7,"-","|");
-        drawKeyboard();
-
+        gotoxy(53,5);
+        cout<<"            "<<endl;
+        gotoxy(53,8);
+        cout<<"            "<<endl;
         gotoxy(53,5);
         cout<<out<<endl;
         gotoxy(53,8);
@@ -57,30 +44,21 @@ void mostCommonWord()
     }
     inputFile.close();
 
-     score=gross-wrong;
+    score=gross-wrong;
     int accuracy=(score*100)/gross;
     system("cls");
-    gotoxy(40,5);
-    cout<<"Total Key Press: "<<gross;
-    gotoxy(40,7);
-    cout<<"Wrong Key Press: "<<wrong;
-    gotoxy(40,9);
-    cout<<"Accuracy: "<<accuracy<<"%";
-    gotoxy(40,12);
+    cout<<"\n\n\n\n\t\t\t\t\t\t Total Key Press: "<<gross;
+    cout<<"\n\n\t\t\t\t\t\t Wrong Key Press: "<<wrong;
+    cout<<"\n\n\t\t\t\t\t\t Accuracy: "<<accuracy<<"%";
 
     ofstream performance("PerformanceHistory.txt",ios::app);
-    performance<<"Common Word \t";
-    performance<<DateFind()<<" \t";
-    performance<<gross<<"\t\t";
-    performance<<wrong<<"\t\t";
-    performance<<accuracy<<"%"<<"\n\n";
+    string write="Common Word,"+DateFind()+","+to_string(gross)+","+to_string(wrong)+","+to_string(accuracy)+"%\n";
+    performance<<write;
     performance.close();
 
+    cout<<"\n\n\t\t\t\t\t\t Press 1 to return home or any key exit.";
 
-    cout<<"Press 1 to return home or any key exit.";
-
-    char temp;
-    temp=getch();
+    char temp=getch();
     if(temp=='1')
         startMenu();
     else
